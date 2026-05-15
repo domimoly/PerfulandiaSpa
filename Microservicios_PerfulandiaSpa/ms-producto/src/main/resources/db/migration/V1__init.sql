1fd97a2d3ed98a279de4222cd797571bd88d024a
molly <dom.ruiz@duocuc.cl>
2026-05-15 02:23:53 -0400

    Modificacion scripts

--- Microservicios_PerfulandiaSpa/ms-producto/src/main/resources/db/migration/V1__init.sql
+++ Microservicios_PerfulandiaSpa/ms-producto/src/main/resources/db/migration/V1__init.sql
@@ -3,20 +3,21 @@ create table producto (
     nombre varchar(255),
     descripcion varchar(255),
     precio double,
+    cantidad integer
     primary key (id)
 );
 
-INSERT INTO productos (nombre, descripcion, precio) 
-VALUES ('Carolina Herrera Good Girl', 'Perfume floral oriental para mujer, 80ml', 115000.0);
+INSERT INTO productos (nombre, descripcion, precio, cantidad) 
+VALUES ('Carolina Herrera Good Girl', 'Perfume floral oriental para mujer, 80ml', 115000.0, 50);
 
-INSERT INTO productos (nombre, descripcion, precio) 
-VALUES ('Dior Sauvage', 'Fragancia aromática fougère para hombre, 100ml', 130000.0);
+INSERT INTO productos (nombre, descripcion, precio, cantidad) 
+VALUES ('Dior Sauvage', 'Fragancia aromática fougère para hombre, 100ml', 130000.0, 40);
 
-INSERT INTO productos (nombre, descripcion, precio) 
-VALUES ('Calvin Klein CK One', 'Aroma cítrico y fresco unisex, 200ml', 45000.0);
+INSERT INTO productos (nombre, descripcion, precio, cantidad) 
+VALUES ('Calvin Klein CK One', 'Aroma cítrico y fresco unisex, 200ml', 45000.0, 30);
 
-INSERT INTO productos (nombre, descripcion, precio) 
-VALUES ('Hugo Boss Bottled', 'Aroma amaderado especiado clásico, 100ml', 75000.0);
+INSERT INTO productos (nombre, descripcion, precio, cantidad) 
+VALUES ('Hugo Boss Bottled', 'Aroma amaderado especiado clásico, 100ml', 75000.0, 20);
 
-INSERT INTO productos (nombre, descripcion, precio) 
-VALUES ('Lancome La Vie Est Belle', 'Fragancia dulce y floral para mujer, 50ml', 95000.0);
+INSERT INTO productos (nombre, descripcion, precio, cantidad) 
+VALUES ('Lancome La Vie Est Belle', 'Fragancia dulce y floral para mujer, 50ml', 95000.0, 10);
