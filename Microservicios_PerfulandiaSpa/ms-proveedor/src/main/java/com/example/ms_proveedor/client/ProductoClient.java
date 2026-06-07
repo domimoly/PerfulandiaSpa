d406038877576f3f8b59f0028bbe585756c7d9c8
molly <dom.ruiz@duocuc.cl>
2026-06-07 18:21:00 -0400

    Avance: Agregar versión URL en conexiones.

--- Microservicios_PerfulandiaSpa/ms-proveedor/src/main/java/com/example/ms_proveedor/client/ProductoClient.java
+++ Microservicios_PerfulandiaSpa/ms-proveedor/src/main/java/com/example/ms_proveedor/client/ProductoClient.java
@@ -14,7 +14,7 @@ public class ProductoClient {
 
     private final WebClient webClient;
 
-    private final String BASE_URL = "http://localhost:8088/api/productos/";
+    private final String BASE_URL = "http://localhost:8088/api/v2/productos/";
 
     public ProductoResponse obtenerProducto(Long id, String token) {
         ApiResponse<ProductoResponse> response = webClient.get()
